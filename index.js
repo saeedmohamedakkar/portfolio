26ee35e4be1975ae47eef2b345bd99f64488cca1
saeedmohamedakkar <saeed.m6063@yahoo.com>
2024-01-24 14:09:57 +0200

    seconde edit

--- index.js
+++ index.js
@@ -1,22 +1,37 @@
 let darkMode = document.querySelector(".dark")
 
-
-
+let darkM = false
 
 
 
 darkMode.addEventListener("click", () => {
 
-    if (document.body.style.backgroundColor === "black") {
-        document.body.style.backgroundColor = "#eaedf0";
-        document.body.style.color = "#2a2d2f";
-    } else {
-        document.body.style.backgroundColor = "black";
-        document.body.style.color = "#bebebe";
+    if (darkM == false) {
+        darkM = true
+        text = true
+    }
+
+
+    else if (darkM == true) {
+        darkM = false
+        text = false
     }
 
 })
 
+
+darkMode.addEventListener("click", () => {
+    if (darkM == false) {
+        document.body.style.backgroundColor = "#eaedf0"
+        document.body.style.color = "#2a2d2f"
+        darkMode.innerHTML = `<div><i class="fa-regular fa-sun text-warning"></i> </div>`
+    }
+    else if (darkM == true) {
+        document.body.style.backgroundColor = "black"
+        document.body.style.color = "#bebebe"
+        darkMode.innerHTML = `<div><i class="fa-regular fa-moon text-secondary"></i> </div>`
+    }
+})
 
 
 
